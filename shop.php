13a1b6d42b130b7eabb0b55cef80d74de4a44a14
Nicolasjmcrt <jumeaucourtn@gmail.com>
2022-10-30 23:25:06 +0100

    Adding more content for Homepage

--- shop.php
+++ shop.php
@@ -10,7 +10,7 @@ require_once './inc/head_inc.php';
     require_once './inc/header_inc.php';
     require_once './inc/shopnav_inc.php';
     ?>
-    
+
     <div class="container-fluid container-ipad">
         <a class="ipad-section" href="<?= URL ?>category.php?category=iPad">
             <div class="ipad-section-texts">
@@ -40,6 +40,20 @@ require_once './inc/head_inc.php';
             <div class="mac-section-texts">
                 <h2 class="text-center">MacBook Pro 13″</h2>
                 <h3 class="text-center">Pro anywhere.</h3>
+            </div>
+        </a>
+    </div>
+    <div class="container-fluid container-double d-flex">
+        <a class="iphone14-section col-6" href="<?= URL ?>category.php?category=iPhone">
+            <div class="iphone14-section-texts">
+                <h2 class="text-center">iPhone 14</h2>
+                <h3 class="text-center">Big and bigger</h3>
+            </div>
+        </a>
+        <a class="tv4k-section col-6" href="<?= URL ?>category.php?category=TV/Home">
+            <div class="tv4k-section-texts">
+                <h2 class="text-center">Apple TV 4k</h2>
+                <h3 class="text-center">The Apple experience. Cinematic in every sense.</h3>
             </div>
         </a>
     </div>
